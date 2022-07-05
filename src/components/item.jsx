56ed5d174419bc3f580afd47ffca6f009f7765ff
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2022-07-05 14:38:37 -0300

    Alteraçoes Layout

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -6,7 +6,9 @@ export default function Item() {
     { id: 1, nome: "Tenis", valor: 210.2 },
     { id: 2, nome: "Raquete", valor: 1210.2 },
     { id: 3, nome: "Computador", valor: 2210.2 },
-    { id: 4, nome: "Bola", valor: 110.2 }
+    { id: 4, nome: "Bola", valor: 110.2 },
+    { id: 5, nome: "Celular", valor: 1710.2 },
+    { id: 6, nome: "Chocolate", valor: 2.2 }
   ];
   const { carrinho, setCarrinho } = useContext(AuthContext);
   function handleClick(item) {
@@ -16,22 +18,24 @@ export default function Item() {
     setCarrinho([]);
   }
   return (
-    <>
+    <div>
       <button onClick={handleDelete} className="delete_cart">
         Limpar Carrinho
       </button>
-      {itens.map((item) => {
-        return (
-          <div className="item__container" key={item.id}>
-            <p>{item.nome}</p>
-            <p>R${item.valor}</p>
+      <div className="itens__container">
+        {itens.map((item) => {
+          return (
+            <div className="item__container" key={item.id}>
+              <p>{item.nome}</p>
+              <p>R${item.valor}</p>
 
-            <div>
-              <button onClick={() => handleClick(item)}>Adicionar</button>
+              <div>
+                <button onClick={() => handleClick(item)}>Adicionar</button>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </>
+          );
+        })}
+      </div>
+    </div>
   );
 }
